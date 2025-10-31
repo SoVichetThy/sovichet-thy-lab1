fa044e949f19de3203db852511f64c6f75f11540
Sovichet Thy <sovichetthy@Sovichet-Macbook.local>
2025-10-31 11:53:59 +0700

    update-message

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ it('should return Hello, GitHub Actions!', async () => {
 const res = await request(app).get('/');
 
 expect(res.status).to.equal(200);
-expect(res.text).to.equal('Hello, GitHub Actions!');
+expect(res.text).to.equal('Hello, CI/CD!');
 });
 });
 // import request from 'supertest';
